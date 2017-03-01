2e66d3ca70a3ac998699d658e641cce812dbd12e
Jonathan Lehr <jonathan@aboutobjects.com>
2017-03-01 15:01:48 -0500

    Coolness2

---
+++ demos/Coolness2/Coolness2/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.yellow
+        window?.rootViewController = CoolViewController(nibName: "CoolStuff", bundle: nil)
+        
+        window?.makeKeyAndVisible()
+    }
+}
+
+
+// MARK: - UIResponder
+extension AppDelegate
+{
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIApplication.shared.sendAction(#selector(resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
